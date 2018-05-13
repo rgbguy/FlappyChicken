798408f7c92574f76a82f52c891f72ed2daaa7cc
sagarkalbande <sagarkalbande1993@gmail.com>
2018-05-14 01:12:50 +0530

    Character Dies when falls

---
+++ Assets/Scripts/PitOfDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PitOfDeath : MonoBehaviour {
+
+
+    void OnTriggerExit2D(Collider2D theCollision)
+    {
+        if(theCollision.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
